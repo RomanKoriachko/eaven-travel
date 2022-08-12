f3bc57c4fdc21cea674e6b54569a5d1e761c1858
Roman <mazaxaka.tyt@gmail.com>
2022-08-12 13:14:26 +0300

    setap for React

---
+++ src/container/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return <div>App</div>
+}
+
+export default App
