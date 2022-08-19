3d0142fed873e96ed091946b92bf39f7d860d4cf
Roman <mazaxaka.tyt@gmail.com>
2022-08-19 13:25:51 +0300

    fix tabs

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
 import './App.scss'
+// import SliderComponent from 'components/SliderComponent/SliderComponent'
 
 type Props = {}
 
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,8 +1,47 @@
-import ArticleHeader from 'components/ArticleHeader/ArticleHeader'
 import React from 'react'
+import ArticleHeader from 'components/ArticleHeader/ArticleHeader'
 import './Header.scss'
+import { render } from '@testing-library/react'
 
 type Props = {}
+
+function tabs() {
+    let tabsItem = document.querySelectorAll('.tab-title-item')
+    let tabsContent = document.querySelectorAll('.tab-content')
+    let tabsWrap = document.querySelector('.tab-title-items')
+
+    function hideTabsContent() {
+        tabsContent.forEach((tab) => {
+            tab.classList.add('hide')
+            tab.classList.remove('show-tab')
+        })
+
+        tabsItem.forEach((item) => {
+            item.classList.remove('active-item')
+        })
+    }
+
+    function showTabsContent(i = 0) {
+        tabsContent[i].classList.add('show-tab')
+        tabsContent[i].classList.remove('hide')
+        tabsItem[i].classList.add('active-item')
+    }
+
+    hideTabsContent()
+    showTabsContent()
+
+    tabsWrap!.addEventListener('click', function (e) {
+        let target = e.target as Element
+        if (target && target.classList.contains('tab-title-item')) {
+            tabsItem.forEach((item, i) => {
+                if (target === item) {
+                    hideTabsContent()
+                    showTabsContent(i)
+                }
+            })
+        }
+    })
+}
 
 const Header = (props: Props) => {
     return (
@@ -462,6 +501,7 @@ const Header = (props: Props) => {
                     </div>
                 </div>
             </header>
+            {tabs()}
         </>
     )
 }
