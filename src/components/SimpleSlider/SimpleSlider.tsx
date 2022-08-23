d00ae3742f56a7a25bb2a0763a27baf91eb6e477
Roman <mazaxaka.tyt@gmail.com>
2022-08-23 20:05:22 +0300

    fix deploing issues

--- src/components/SimpleSlider/SimpleSlider.tsx
+++ src/components/SimpleSlider/SimpleSlider.tsx
@@ -4,8 +4,6 @@ import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
 import './SimpleSlider.scss'
 
-type Props = {}
-
 let windowHeight = window.innerHeight
 console.log(windowHeight)
 
